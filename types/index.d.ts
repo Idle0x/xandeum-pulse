94fbb25bad80736bc57acdf6858118c3d4d06431
Idle0x <140549997+Idle0x@users.noreply.github.com>
2025-12-19 00:41:26 +0100

    Create index.d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,24 @@
+declare module 'react-simple-maps' {
+  import * as React from 'react';
+
+  export interface ComposableMapProps {
+    width?: number;
+    height?: number;
+    projection?: string | Function;
+    projectionConfig?: object;
+    className?: string;
+    style?: object;
+    children?: React.ReactNode;
+  }
+
+  export const ComposableMap: React.FC<ComposableMapProps>;
+  export const ZoomableGroup: React.FC<any>;
+  export const Geographies: React.FC<any>;
+  export const Geography: React.FC<any>;
+  export const Marker: React.FC<any>;
+}
+
+declare module 'd3-scale' {
+  export function scaleSqrt(): any;
+  export function scaleLinear(): any;
+}
