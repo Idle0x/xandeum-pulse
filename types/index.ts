4f4ad515b0e57c65aa8143963fd4c99782c0b444
Idle0x <140549997+Idle0x@users.noreply.github.com>
2026-01-10 23:41:38 +0100

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,7 @@
 // types/index.ts
+
 export interface Node {
+  uid?: string; // <--- NEW: Unique ID (Composite of Pubkey + Network)
   address?: string;
   pubkey?: string;
   version?: string;
@@ -33,4 +35,33 @@ export interface Node {
     mainnetCount: number;
     devnetCount: number;
   };
+}
+
+// --- MODULE DECLARATIONS ---
+
+declare module 'react-simple-maps' {
+  import * as React from 'react';
+
+  export interface ComposableMapProps {
+    width?: number;
+    height?: number;
+    projection?: string | Function;
+    projectionConfig?: object;
+    className?: string;
+    style?: object;
+    children?: React.ReactNode;
+  }
+
+  export const ComposableMap: React.FC<ComposableMapProps>;
+  export const ZoomableGroup: React.FC<any>;
+  export const Geographies: React.FC<any>;
+  export const Geography: React.FC<any>;
+  export const Marker: React.FC<any>;
+  export const Line: React.FC<any>;
+  export const Annotation: React.FC<any>;
+}
+
+declare module 'd3-scale' {
+  export function scaleSqrt(): any;
+  export function scaleLinear(): any;
 }
